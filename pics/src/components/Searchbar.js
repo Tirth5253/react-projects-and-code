16ccc539edbd6e00ea896679b15a202d58021109
Tirth Joshi <tirthjoshi5253@gmail.com>
2023-06-07 23:55:55 +0530

    fifth commit

--- pics/src/components/Searchbar.js
+++ pics/src/components/Searchbar.js
@@ -1,15 +1,22 @@
 function Searchbar({onSubmit}){
 
-const handleClick=()=>{
+const handleFormSubmit=(event)=>{
+  event.preventDefault()
   onSubmit('cars')
 };
 
     return <div>
+      <form  onSubmit={handleFormSubmit}>
       <input/>
-      <button onClick={handleClick}>click me</button>
+      </form>
+      
+      
     </div>
   }
   export default Searchbar;
 
 
   //here we have recieved  the prop called onSubmit from the App(parent)   
+  //2... we have entered "event" in the event handler bcse when we write our code in the "FORM" so this will couse 
+  //our page to go reload every time when we use the form.. so ".preventDeafult" will couse not to go
+  
