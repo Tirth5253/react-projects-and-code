b5aa395ba8a2d01b3a492efd32b25b079dafd02c
Tirth Joshi <tirthjoshi5253@gmail.com>
2023-06-08 23:03:48 +0530

    sixthCommit

--- pics/src/components/Searchbar.js
+++ pics/src/components/Searchbar.js
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 function Searchbar({onSubmit}){
 
 const handleFormSubmit=(event)=>{
   event.preventDefault()
-  onSubmit('cars')
+  onSubmit(term)
+
+//NEVER WRITE THIS FOR REACT
+//document.querySelector('input').value; for the value of input from the user bcse of forms' component wierdness
+//in the react
+//bcse this text can be used for way many things 
+
 };
+const [term,setTerm]= useState('');
+
+const handleChange=(event)=>{
+  setTerm(event.target.value);  //this will change(set) our term to the input by the user as
+                                //onchange==event  value== input by user and target is the catagory
+                                //on further details that how we did refer the notes
+  
+}
+
 
     return <div>
       <form  onSubmit={handleFormSubmit}>
-      <input/>
+      <input value={term}  onChange={handleChange}/>
       </form>
       
       
@@ -19,4 +36,6 @@ const handleFormSubmit=(event)=>{
   //here we have recieved  the prop called onSubmit from the App(parent)   
   //2... we have entered "event" in the event handler bcse when we write our code in the "FORM" so this will couse 
   //our page to go reload every time when we use the form.. so ".preventDeafult" will couse not to go
+
+//NOTE we will write the "event" as a parametre every time we pass the eventHndler in pure HTML compo.
   
