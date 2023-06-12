930542496375da4b8001a8330ed9f0b28cdbfe03
Tirth Joshi <tirthjoshi5253@gmail.com>
2023-06-12 23:57:28 +0530

    ninth commit

--- pics/src/components/imageList.js
+++ pics/src/components/imageList.js
@@ -3,8 +3,10 @@ import ImageShow from './imageShow';
 function imageList({images}){
 //by bolow mapping function we will print or get the each indivisual image from the Array
 const RenderedImages=images.map((image)=>{
-
-  return <ImageShow image={image}/>
+ 
+  return<div key={image.id}>
+    <ImageShow  image={image}/>
+        </div>
 });
 
 
--- pics/src/components/imageShow.js
+++ pics/src/components/imageShow.js
@@ -1,4 +1,4 @@
 function imageShow({image}){
-    return <div>{image.id}</div>
+    return <div>{image.alt_description}</div>
   }
   export default imageShow;
