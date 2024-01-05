82583dd60d911b2c45211bf5897f5142bc03c783
Tirth Joshi <tirthjoshi5253@gmail.com>
2024-01-05 22:17:59 +0530

    forth

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import {GoChevronDown} from 'react-icons/go';
  
 
@@ -6,6 +6,28 @@ import {GoChevronDown} from 'react-icons/go';
 function Dropdown({options,selection,onSelect}){
 
 const [isOpen,setIsOpen]=useState(false);//this state is res. for the options to get open or closed
+
+
+
+const  delEl=useRef();//for more deatils about this refer notes
+
+//this below code is for when user clicks anywhere besides dropdown the dropdown should be closed for more refer note
+//the return functions ensures that if our drpdown get deleted then the this fn will stop the code to executes if user clicks on any where else bcs then it will be waste of time
+useEffect(()=>{
+const handle=(event)=>{
+if(!delEl.current.contains(event.target)){
+   setIsOpen(false);
+}
+};
+document.addEventListener('click',handle,true);
+return()=>{
+   document.removeEventListener('click',handle)
+}
+},[])
+
+
+
+
 
 const handleClick=()=>{
    setIsOpen(!isOpen)           //so this will cauese what the opp. of  the isOpen so it will close if open or just open if closed
@@ -27,7 +49,7 @@ return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={()=
 });   
 
   //so here is the thing the "selection?.label" will say that if selection is null then it will give undefined so the truthy value "Selected" will go but if thats not then it will retuen label of the option
-   return  (<div className="w-48 relative "> 
+   return  (<div ref={delEl} className="w-48 relative "> 
       <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"  onClick={handleClick}>{selection?.label || 'Selected...'}<GoChevronDown className="text-lg"/></div>
       {isOpen && <div  className="absolute top-full border rounded p-3 shadow bg-white w-full" >{renderedOptions}</div>}
       </div>)
