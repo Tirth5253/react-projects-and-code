53b6b67d213bacf3537bacb2e0547c9496ab235e
Tirth Joshi <tirthjoshi5253@gmail.com>
2024-01-10 21:21:16 +0530

    sidebar

--- comps/src/components/Link.js
+++ comps/src/components/Link.js
@@ -1,4 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+
+import classNames from 'classnames';
 import { useContext } from "react";
 import NavigationContext from "../contex/navigation";
 
@@ -6,12 +8,24 @@ import NavigationContext from "../contex/navigation";
 //the first prop is "to" which is the path we want to navigate
 //the second one is the text as a "children" that we will pass in the "anchor" tag
 
-function Link({to,children}){
-  const {navigate}=  useContext(NavigationContext);//this line says that we will use or import the navigate() function of the NavigationContext
-    const handleClick=(event)=>{
+function Link({to,children,className,activeClassName}){
+  const {navigate,currentPath}=  useContext(NavigationContext);//this line says that we will use or import the navigate() function of the NavigationContext
+   
+
+
+  const classes=classNames('text-blue-500'
+  ,className,
+  currentPath===to&& activeClassName //so this says that if the our current path is the navigation path then we add this class this would cause our link to get highlight
+  );//here className says that we will add some property to link by our own way as a prop
+
+  const handleClick=(event)=>{
+if(event.metaKey || event.ctrlKey){ //this code will basically says that if click using holding the ctrl key than this navigation happens in another page
+    return; 
+}
+
       event.preventDefault();
-     navigate(to)
+     navigate(to) 
     };
-return <a onClick={handleClick}>{children}</a>
+return <a href={to} onClick={handleClick}  className={classes}>{children}</a>
 }
 export default Link;
